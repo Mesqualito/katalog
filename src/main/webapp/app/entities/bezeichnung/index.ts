35a9ada32edc72b6a0c9a41255cf22fa80fdb0d5
Jochen Haßfurter <jo@ateliermo.de>
2019-02-11 17:33:38 +0100

    Initial application generated by JHipster-5.8.1

--- src/main/webapp/app/entities/bezeichnung/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './bezeichnung.service';
-export * from './bezeichnung-update.component';
-export * from './bezeichnung-delete-dialog.component';
-export * from './bezeichnung-detail.component';
-export * from './bezeichnung.component';
-export * from './bezeichnung.route';
--- src/main/webapp/app/shared/model/sprache.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ISprache {
-    id?: string;
-    sprachCode?: string;
-    sprachBezeichnung?: string;
-}
-
-export class Sprache implements ISprache {
-    constructor(public id?: string, public sprachCode?: string, public sprachBezeichnung?: string) {}
-}
