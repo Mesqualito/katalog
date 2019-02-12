db06c544e5451d48a7c081fcb0d92b1792269747
Jochen <jo@hassfurter.com>
2019-02-12 10:47:24 +0100

    Initial application generated by JHipster-5.8.1

--- src/main/webapp/app/entities/gruppe/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './gruppe.service';
-export * from './gruppe-update.component';
-export * from './gruppe-delete-dialog.component';
-export * from './gruppe-detail.component';
-export * from './gruppe.component';
-export * from './gruppe.route';
--- src/main/webapp/app/shared/model/sprache.model.ts
+++
@@ -1,9 +0,0 @@
-export interface ISprache {
-    id?: number;
-    sprachCode?: string;
-    sprachBezeichnung?: string;
-}
-
-export class Sprache implements ISprache {
-    constructor(public id?: number, public sprachCode?: string, public sprachBezeichnung?: string) {}
-}
